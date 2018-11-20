6d29569b06d66958f2262d0926e4e828f42c9d92
jefftorcato <jefftorcato@gmail.com>
2018-11-20 16:55:42 +0530

    Fix for input delay

--- src/gamemain/KeyInput.java
+++ src/gamemain/KeyInput.java
@@ -6,9 +6,13 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter{
 
 	private Handler handler;
+	private boolean[] keyDown = new boolean[4];
 	
 	public KeyInput(Handler handler) {
 		this.handler = handler;
+		for (int i = 0; i < keyDown.length; i++) {
+			keyDown[i] = false;
+		}
 	}
 	
 	public void keyPressed(KeyEvent e) {
@@ -20,10 +24,22 @@ public class KeyInput extends KeyAdapter{
 			
 			if(tempObject.getId() == ID.Player) {
 				//Key events for player one
-				if(key == KeyEvent.VK_W) tempObject.setVelY(-5);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(5);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(5);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(-5);
+				if(key == KeyEvent.VK_W) { 
+					tempObject.setVelY(-5);
+					keyDown[0] = true;
+				}
+				if(key == KeyEvent.VK_S) {
+					tempObject.setVelY(5);
+					keyDown[1] = true;
+				}
+				if(key == KeyEvent.VK_D) {
+					tempObject.setVelX(5);
+					keyDown[2] = true;
+				}
+				if(key == KeyEvent.VK_A) {
+					tempObject.setVelX(-5);
+					keyDown[3] = true;
+				}
 			}
 			/*if(tempObject.getId() == ID.Player2) {
 				//Key events for player two
@@ -44,10 +60,15 @@ public class KeyInput extends KeyAdapter{
 			
 			if(tempObject.getId() == ID.Player) {
 				//Key events for player one
-				if(key == KeyEvent.VK_W) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(0);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(0);
+				if(key == KeyEvent.VK_W) keyDown[0] = false; //tempObject.setVelY(0);
+				if(key == KeyEvent.VK_S) keyDown[1] = false; //tempObject.setVelY(0);
+				if(key == KeyEvent.VK_D) keyDown[2] = false; //tempObject.setVelX(0);
+				if(key == KeyEvent.VK_A) keyDown[3] = false; //tempObject.setVelX(0);
+			
+				//vertical movement
+				if(!keyDown[0] && !keyDown[1]) tempObject.setVelY(0);
+				//Horizontal movement
+				if(!keyDown[2] && !keyDown[3]) tempObject.setVelX(0);
 			}
 			/*if(tempObject.getId() == ID.Player2) {
 				//Key events for player two
